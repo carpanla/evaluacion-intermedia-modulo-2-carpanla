8533e2949ecb9d254ef8a13f842eab9babd67baa
carpanla <carpanla@gmail.com>
2019-11-20 16:25:08 +0100

    add function compare numbers

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 'use strict';
 
+const inputNumber = document.querySelector('.form__input').value;
 const button = document.querySelector('.form__button');
-const inputNumber = document.querySelector('.form__input');
-let counter = 0;
 const clue = document.querySelector('.text__clue');
 const textCounter = document.querySelector('.text__counter');
+let counter = 0;
 
 //Primero hay que generar el número aleatorio
 
@@ -15,22 +15,28 @@ function getRandomNumber(max) {
 const randomNumber = getRandomNumber(100);
 console.log(randomNumber);
 
-button.addEventListener('click', getRandomNumber);
-
 //Necesito comparar el número introducido con el número generado, ya que en función de eso aparecerá un texto u otro.
 
 function compareNumbers () {
-    if (inputNumber > 100){
+    console.log('llamando');
+    if (inputNumber > 100) {
         clue.innerHTML = "El número debe estar entre 1 y 100.";
     }
     else if (inputNumber < 1) {
         clue.innerHTML = "El número debe estar entre 1 y 100.";
     }
     else if (inputNumber > randomNumber) {
-        clue.innerHTML = "El número es demasiado bajo";
+        clue.innerHTML = "El número es demasiado alto.";
     }
     else if (inputNumber < randomNumber) {
-        clue.innerHTML = "El número es demasiado bajo";
+        clue.innerHTML = "El número es demasiado bajo.";
     }
-    console.log(compare());
 }
+
+button.addEventListener('click', compareNumbers);
+
+//Hago la función contador para que me indique los intentos que me quedan
+
+
+
+button.addEventListener('click', getRandomNumber);
